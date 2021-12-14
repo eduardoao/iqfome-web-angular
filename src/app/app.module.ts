7ac1c4d0515cacfe61ca35dc99da00fa8a1f1ddb
Eduardo Alcantara de Oliveira <eoliveira@trt4.jus.br>
2021-12-14 10:47:38 -0300

    Created initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HeaderComponent } from './header/header.component';
+import { HeadermenuComponent } from './headermenu/headermenu.component';
+import { HeadercartComponent } from './headercart/headercart.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { SearchComponent } from './search/search.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { CardComponent } from './components/card/card.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HeaderComponent,
+    HeadermenuComponent,
+    HeadercartComponent,
+    CategoriesComponent,
+    SearchComponent,
+    CarouselComponent,
+    CardComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/headercart/headercart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-headercart',
+  templateUrl: './headercart.component.html',
+  styleUrls: ['./headercart.component.css']
+})
+export class HeadercartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
